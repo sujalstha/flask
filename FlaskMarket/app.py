125b6e2e8aff3d6634428b456db6d81b7174e216
Sujal Shrestha <sujalshrestha2004@gmail.com>
2021-06-22 16:08:17 -0500

    Commit initial

--- FlaskMarket/app.py
+++ FlaskMarket/app.py
@@ -5,4 +5,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return '<h1>Hello World!<h1>'
+    return '<h1>Hello World</h1>'
+
+
+@app.route('/about/<username>')
+def about_page(username):
+    return f'<h1>This is the about page of {username}'
