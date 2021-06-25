da97f39b2e527e3962f70098009af3141a256f3a
Sujal Shrestha <sujalshrestha2004@gmail.com>
2021-06-25 15:14:23 -0500

    Commit initial

--- app_market.py
+++ app_market.py
@@ -1,11 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return '<h1>Hello, Welcome to the Flask Market.<h1>'
+    return render_template('home.html')
+
+
+'''@app.route('/')
+def hello_world():
+    return '<h1>Hello, Welcome to the Flask Market.<h1>'''''
 
 
 @app.route('/about/<username>')
