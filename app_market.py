9bf47de07040281f9fe0820c2670c0e6456459bd
Sujal Shrestha <sujalshrestha2004@gmail.com>
2021-06-26 17:01:47 -0500

    Commit initial

--- app_market.py
+++ app_market.py
@@ -17,8 +17,8 @@ def hello_world():
 def market_page():
     items = [
         {'id': 1, 'name': 'Phone', 'barcode': '893212299897', 'price': 500},
-        {},
-        {},
+        {'id': 2, 'name': 'Laptop', 'barcode': '123985473165', 'price': 900},
+        {'id': 3, 'name': 'Keyboard', 'barcode': '231985128446', 'price': 150},
     ]
     return render_template('market.html', item_name='Phone')
 
