e4bf91e015c4765a423d1fc52d2fec1718c38689
Sujal Shrestha <sujalshrestha2004@gmail.com>
2021-06-25 22:09:00 -0500

    Commit initial

--- app_market.py
+++ app_market.py
@@ -13,6 +13,16 @@ def hello_world():
     return '<h1>Hello, Welcome to the Flask Market.<h1>'''''
 
 
+@app.route('/market')
+def market_page():
+    items = [
+        {'id': 1, 'name': 'Phone', 'barcode': '893212299897', 'price': 500},
+        {},
+        {},
+    ]
+    return render_template('market.html', item_name='Phone')
+
+
 @app.route('/about/<username>')
 def about_page(username):
     return f'<h1>About Page of [ {username} ] <h1>'
