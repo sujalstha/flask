ffaf36fdc4f89495e69acbdbfbcba71300d8d32c
Sujal Shrestha <sujalshrestha2004@gmail.com>
2021-06-27 14:14:54 -0500

    Commit initial

--- app_market.py
+++ app_market.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template
-from flask_sqlachemy import SQLAlchemy
+
 app = Flask(__name__)
+db = SQLAlchemy(app)
+
+class Item(db.Model):
+    name = db.Column(db.String(length=30))
 
 @app.route('/')
 @app.route('/home')
