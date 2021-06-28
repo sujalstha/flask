127965375f349855451b3e857f92991ce92e032f
Sujal Shrestha <sujalshrestha2004@gmail.com>
2021-06-27 20:51:34 -0500

    Commit initial

--- app_market.py
+++ app_market.py
@@ -4,7 +4,6 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
 db = SQLAlchemy(app)
 
-
 class Item(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
     name = db.Column(db.String(length=30), nullable=False, unique=True)
